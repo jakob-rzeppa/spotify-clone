4d1f7e307f5bcd540ea35ee41ce10475dab1f17a
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-02-03 10:44:26 +0100

    search functionality - 03:41

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,7 +41,7 @@ const Header: React.FC<HeaderProps> = ({
     }
 
     return (
-        <div className={twMerge("h-fit bg-gradient-to-b from-emerald-800 p-6")}>
+        <div className={twMerge("h-fit bg-gradient-to-b from-emerald-800 p-6", className)}>
             <div className="w-full mb-4 flex items-center justify-between">
                 <div className="hidden md:flex gap-x-2 items-center">
                     <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition" onClick={() => router.back}>
