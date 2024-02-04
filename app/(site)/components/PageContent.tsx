d626bf8d4334dba43b82832321a8792dfa0ca37a
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-02-04 11:35:24 +0100

    sound bar no functionality - 04:40

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Song } from "@/types";
+
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface PageContentProps {
     songs: Song[];
@@ -10,6 +12,8 @@ interface PageContentProps {
 const PageContent: React.FC<PageContentProps> = ({
     songs
 }) => {
+    const onPlay = useOnPlay(songs);
+
     if (songs.length === 0) {
         return (
             <div className="mt-4 text-neutral-400">
@@ -21,7 +25,7 @@ const PageContent: React.FC<PageContentProps> = ({
     return (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
             {songs.map((item) => (
-                <SongItem key={item.id} onClick={() => {}} data={item}/>
+                <SongItem key={item.id} onClick={(id: string) => onPlay(id)} data={item}/>
             ))}
         </div>
     );
