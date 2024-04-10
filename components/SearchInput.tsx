b16a496c7f73d834016aa92722d268f84fab4d9f
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-10 08:46:09 +0200

    reformatting

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -8,26 +8,30 @@ import useDebounce from "@/hooks/useDebounce";
 import Input from "./Input";
 
 const SearchInput = () => {
-    const router = useRouter();
-    const [value, setValue] = useState<string>("");
-    const debouncedValue = useDebounce<string>(value, 500);
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
 
-    useEffect(() => {
-        const query = {
-            title: debouncedValue,
-        };
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
 
-        const url = qs.stringifyUrl({
-            url: "/search",
-            query: query
-        });
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
 
-        router.push(url);
-    }, [debouncedValue, router])
+    router.push(url);
+  }, [debouncedValue, router]);
 
-    return (
-        <Input placeholder="What do you want to listen to?" value={value} onChange={(e) => setValue(e.target.value)}/>
-    );
-}
- 
+  return (
+    <Input
+      placeholder="What do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
+
 export default SearchInput;
